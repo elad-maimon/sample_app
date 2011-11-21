34f7caf42adecae864bb2d6b9330e9a362a59575
Elad Maimon <elad.maimon@klarna.com>
2011-11-21 16:47:08 +0200

    Finish chapter 6

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -25,4 +25,41 @@ describe "LayoutLinks" do
     get '/signup'
     response.should have_selector('title', :content => "Sign up")
   end
+
+  it "should have the right link for About page" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+  end
+
+  it "should have the right link for Help page" do
+    visit root_path
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+  end
+
+  it "should have the right link for Contact page" do
+    visit root_path
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+  end
+
+  it "should have the right link for Home page" do
+    visit root_path
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+  end
+/#
+  it "should have the right link for Sign in page" do
+    visit root_path
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign in")
+  end
+
+  it "should have the right link for Sign up button" do
+    visit root_path
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign up")
+  end
+#/
 end
